57ece4d0e7abad5d3374cfdb4a5790860703d48a
Reguna <60984878+regunakyle@users.noreply.github.com>
2024-08-05 23:23:23 +0800

    Use vfox

--- scripts/setup_windows_vm.ps1
+++ scripts/setup_windows_vm.ps1
@@ -59,8 +59,11 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://com
 
 # Install packages
 Write-Host "Installing common packages..."
-choco install -y python 7zip.install vlc.install microsoft-windows-terminal imageglass notepadplusplus.install nano-win `
-    libreoffice-fresh zulu powertoys foobar2000 itunes gsudo cheatengine chocolateygui qbittorrent
+choco install -y 7zip.install vlc.install microsoft-windows-terminal imageglass notepadplusplus.install nano-win `
+    libreoffice-fresh powertoys foobar2000 itunes gsudo cheatengine chocolateygui qbittorrent
+
+winget install vfox
+if (-not (Test-Path -Path $PROFILE)) { New-Item -Type File -Path $PROFILE -Force }; Add-Content -Path $PROFILE -Value 'Invoke-Expression "$(vfox activate pwsh)"'
 
 # SSH server for ProxyJump
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
