04ef5f711f9799a3b6b7ffd8a43e1f13527be2a8
Reguna <60984878+regunakyle@users.noreply.github.com>
2024-11-17 04:44:14 +0800

    Windows use winget instead of choco

--- scripts/setup_windows_vm.ps1
+++ scripts/setup_windows_vm.ps1
@@ -1,11 +1,13 @@
 <# Instruction
-0. Finish all available Windows update first (Preferably also activiate Windows)
-1. Start an elevated Powershell prompt, run `Set-ExecutionPolicy Bypass -Scope Process -Force`,
-then run this script
+0. Finish all available Windows update (Preferably also activiate Windows)
+1. Update the `App Installer` in Microsoft Store
+2. Start an elevated Powershell prompt, run `Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force`, then run this script
 
 TODO: 
 - Add error handling
+- Automate winget installation with https://github.com/microsoft/winget-cli/discussions/1738#discussioncomment-5484927
 #>
+
 if (Test-Path env:TEMP) {
     Push-Location $env:TEMP
 }
@@ -19,7 +21,7 @@ else {
 
 # Uninstall bloatware
 Write-Host "Uninstalling bloatware..."
-$bloatware = @(
+$bloatwares = @(
     "Microsoft.Microsoft3DViewer",
     "Microsoft.WindowsAlarms",
     # Mail, Calendar
@@ -49,24 +51,52 @@ $bloatware = @(
     "Microsoft.WindowsCamera"
 )
 
-foreach ($item in $bloatware) {
-    Get-AppxPackage $item | Remove-AppxPackage
+foreach ($bloatware in $bloatwares) {
+    Get-AppxPackage $bloatware | Remove-AppxPackage
 }
 
-# Install Chocolatey
-Write-Host "Installing Chocolatey..."
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Check for winget
+try {
+    # Powershell try-catch works only when the error is terminating
+    Get-Command winget -ErrorAction Stop
+}
+catch {
+    Write-Host "Winget not found, please update the `App Installer` in Microsoft Store!"
+    exit 1
+}
+
 
 # Install packages
 Write-Host "Installing common packages..."
-choco install -y 7zip.install vlc.install imageglass notepadplusplus.install nano-win firefox `
-    libreoffice-fresh powertoys foobar2000 temurin itunes gsudo python chocolateygui qbittorrent
+$packages = @(
+    "7zip.7zip",
+    "Apple.iTunes",
+    "DuongDieuPhap.ImageGlass",
+    "Git.Git",
+    "gerardog.gsudo",
+    "Microsoft.PowerToys",
+    "Microsoft.WindowsTerminal",
+    "Mozilla.Firefox",
+    "Notepad++.Notepad++",
+    "okibcn.nano",
+    "qBittorrent.qBittorrent",
+    "TheDocumentFoundation.LibreOffice",
+    "version-fox.vfox",
+    "VideoLAN.VLC"
+)
+
+foreach ($package in $packages) {
+    winget install --id=$package -e --accept-package-agreements --accept-source-agreements --source winget
+}
+
+# Add vfox to shell
+if (-not (Test-Path -Path $PROFILE)) { New-Item -Type File -Path $PROFILE -Force }; Add-Content -Path $PROFILE -Value 'Invoke-Expression "$(vfox activate pwsh)"'
 
 # SSH server for ProxyJump
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
-# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+# Confirm the Firewall rule is configured. It should be created automatically by setup.
 if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
     Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
     New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
@@ -74,33 +104,30 @@ if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyCon
     Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
 }
 
-choco install -y git.install --params "'/GitOnlyOnPath /WindowsTerminalProfile /NoOpenSSH /DefaultBranchName:main /Editor:Nano'"
-
 # Trader Workstation from IBKR
 Write-Host "Installing Trader Workstation..."
 Invoke-WebRequest https://download2.interactivebrokers.com/installers/tws/latest/tws-latest-windows-x64.exe `
     -OutFile tws-latest-windows-x64.exe
-$TWD = Start-Process -FilePath .\tws-latest-windows-x64.exe -ArgumentList "-q" -PassThru
-while (!$TWD.HasExited) {
+$install = Start-Process -FilePath .\tws-latest-windows-x64.exe -ArgumentList "-q" -PassThru
+while (!$install.HasExited) {
     Write-Host "Waiting for TWS installation to complete..."
     Start-Sleep -Seconds 3
 }
 Remove-Item .\tws-latest-windows-x64.exe
 
 if ((Get-CimInstance win32_VideoController).Name | Select-String "Nvidia") {
-    # Desktop specific scripts; Assume only desktop uses Nvidia GPU
-    Write-Host "Detected Nvidia GPU. Installing desktop specific apps..."
-    choco install -y discord.install geforce-experience
+    Write-Host "Installing desktop packages..."
+    $packages = @(
+        "Discord.Discord",
+        "Valve.Steam"
+    )
 
-    # Steam
-    Invoke-WebRequest https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe `
-        -OutFile SteamSetup.exe
-    $STEAM = Start-Process -FilePath .\SteamSetup.exe -ArgumentList "/S" -PassThru
-    while (!$STEAM.HasExited) {
-        Write-Host "Waiting for steam install to complete..."
-        Start-Sleep -Seconds 3
+    foreach ($package in $packages) {
+        winget install --id=$package -e --accept-package-agreements --accept-source-agreements --source winget
     }
-    Remove-Item .\SteamSetup.exe
+
+    # https://github.com/microsoft/winget-pkgs/issues/140696
+    Write-Host "You should install the Nvidia App manually."
 
 }
 
