bebeda583be297fa3c1f7d806ac613e4a4860cdb
Eric Leung <contact@ericleung.dev>
2025-12-22 00:47:31 +0800

    cleanup

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,3 @@
-oh-my-posh init pwsh --config "$HOME/Documents/Powershell/powerlevel10k_rainbow.omp.json" | Invoke-Expression
-$Env:VIRTUAL_ENV_DISABLE_PROMPT = $false
-
 mise activate pwsh | Out-String | Invoke-Expression
 
 function call_pip {
@@ -10,5 +7,8 @@ function call_pip {
 Set-Alias pip call_pip
 
 # fzf bindings
-$Env:FZF_DEFAULT_COMMAND = "rg --files --hidden --glob `"!.git/*`""
+$env:FZF_DEFAULT_COMMAND = "rg --files --hidden --glob `"!.git/*`""
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+$env:VIRTUAL_ENV_DISABLE_PROMPT = $false
+oh-my-posh init pwsh --config "$HOME/Documents/Powershell/powerlevel10k_rainbow.omp.json" | Invoke-Expression
